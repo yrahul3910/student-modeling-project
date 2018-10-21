4ab1080e5b530a9807d0eba73e31995245e95e5b
Ankush kumar <ankushk@Ankushs-MacBook-Air.local>
2018-10-21 14:07:07 +0530

    update

--- WebApp/src/app/app.component.ts
+++ WebApp/src/app/app.component.ts
@@ -8,3 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'client-app';
 }
+
+function myfunc(username,password) {
+
+(async () => {
+  const rawResponse = await fetch('/api/user/login', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({a: username, b: password})
+  });
+  const content = await rawResponse.json();
+
+  console.log(content);
+})();
+	
+
+
+}
+
