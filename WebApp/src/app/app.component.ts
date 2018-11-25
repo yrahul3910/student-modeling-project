ef814e831e6185fee2cc2814d0d9d4fa69d042d8
Ankush kumar <ankushk@Ankushs-MacBook-Air.local>
2018-11-25 12:16:56 +0530

    login_signup

--- WebApp/src/app/app.component.ts
+++ WebApp/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+<<<<<<< HEAD
   title = 'client-app';
+=======
+
+	
+>>>>>>> 0e7fb64... login_signup
 }
--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -1,14 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [{
+    path: '',
+    children: [
+      { path: 'signup', component: SignupComponent },
+      { path: 'login',component: LoginComponent}
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes,{ enableTracing: true}),
+    FormsModule
+  ],
+  exports: [
+  	HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
