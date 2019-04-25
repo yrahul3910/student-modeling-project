6166d69c6c280546547c0345775324423c79408a
vimarai97 <vimarai1997@gmail.com>
2019-04-25 18:56:51 +0530

    Added result page functionality

--- WebApp/src/app/result/result.component.ts
+++ WebApp/src/app/result/result.component.ts
@@ -10,6 +10,9 @@ import { ActivatedRoute } from '@angular/router';
 export class ResultComponent implements OnInit {
   student: string;
   concept: string;
+  start_probs = new Array<string>(2);
+  transition_probs = new Array<string>(2);
+  emission_probs = new Array<string>(2);
   constructor(public http: HttpClient, public route: ActivatedRoute) {
     this.route.queryParams.subscribe(params => {
         this.concept = params['concept'];
@@ -24,7 +27,17 @@ export class ResultComponent implements OnInit {
     };
     this.http.post('/api/user/performance', postData, options).subscribe(
       suc => {
-        console.log(suc);
+        if (suc['success']==='true'){
+          for (let i = 0; i < suc['start_probs']['length']; i++) {
+            this.start_probs[i] = suc['start_probs'][i];
+          }
+          for (let i = 0; i < suc['transition_probs']['length']; i++) {
+            this.transition_probs[i] = suc['transition_probs'][i];
+          }
+          for (let i = 0; i < suc['emission_probs']['length']; i++) {
+            this.emission_probs[i] = suc['emission_probs'][i];
+          }
+        }
       },
       err => {
         console.log(err);
